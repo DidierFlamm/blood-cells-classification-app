5cd6bca64017ea0097bd52a795e14008934993a3
Didier Flamm <did_padman@hotmail.com>
2025-07-19 14:02:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ ambiance = st.sidebar.radio(
     (
         "🔇 Silent mode",
         "🎛️ French Connexion",
-        "🕌 Indian Indie Pop",
+        "🕌 Indian Indie Pop Ale",
         "💿 Welcome to Gattaca",
         "💲 Your ad here for...",
     ),
