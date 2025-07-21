b4aa8982777683e8e77ce576c1f82261435c528e
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-07-21 12:14:51 +0200

    remove last app description...

--- pages/2_visualisation.py
+++ pages/2_visualisation.py
@@ -7,43 +7,7 @@ st.markdown(
 )
 
 
-st.subheader(":red[Analyse univariée]", divider=True)
-
-st.write(
-    """
-    L’analyse univariée consiste à examiner **chaque variable séparément**, sans tenir compte des autres. 
-    Elle permet de comprendre la **répartition** des données, de détecter d’éventuels **déséquilibres**, 
-    ou encore d’identifier des **outliers**, c'est à dire des valeurs extrêmes (statistiquement éloignées) ou aberrantes (souvent erronées).
-
-    👉 Chaque onglet ci-dessous présente une **visualisation unique** de la répartition de la **variable cible** (survie),
-    ainsi que des **différentes caractéristiques** (âge, sexe, classe, tarif, etc.).
-
-    Cette étape est essentielle pour avoir une première idée de la **structure des données** avant de passer 
-    à des analyses plus complexes (bivariées ou multivariées) et enfin à la modélisation prédictive.
-    """
-)
-
-
-st.subheader(":red[Analyse bivariée]", divider=True)
-
-st.write(
-    "L’analyse bivariée consiste à étudier la **relation entre deux variables** afin de comprendre comment elles interagissent ou sont liées. C’est une étape clé pour explorer un dataset avant de modéliser. "
-)
-st.write(
-    "• L'analyse **target/feature** permet d’étudier comment une variable explicative (feature) est liée à la variable cible (target)."
-)
-st.write(
-    "• L'analyse **feature/feature** permet d'explorer la relation entre 2 variables explicatives. Cela peut aider à détecter des dépendances, interactions, ou colinéarités qui influencent la modélisation."
-)
-
-st.subheader(":red[Analyse multivariée]", divider=True)
-
-st.write(
-    "L’analyse multivariée étudie simultanément les relations entre trois variables ou plus afin de mieux comprendre la structure complexe des données."
-)
-st.write(
-    "Il existe de nombreuses méthodes d'analyse multivariée permettant de détecter les interactions, réduire la dimensionnalité ou encore segmenter les observations (ACP, AFC, clustering, etc...) mais nous n'aborderons ici que 2 visualisations multivariées par graphiques interactifs"
-)
+st.subheader(":red[subheader]", divider=True)
 
 
 _, col, _ = st.columns(3)
@@ -67,7 +31,7 @@ st.markdown(
     """
     <div style='text-align: center; font-size: small; color: gray;'>
     © 2023 Jonathan CAMBON, Didier FLAMM, Mamadou HASSIMIOU BAH, Ilyass SAIDI, supervised by Khalil OUERTANI<br>
-    © 2025 Didier FLAMM, Pavithra BHASKAR
+    © 2025 Didier FLAMM feat. Pavithra BHASKAR
     </div>
     """,
     unsafe_allow_html=True,
--- pages/4_optimization.py
+++ pages/4_optimization.py
@@ -39,7 +39,7 @@ st.markdown(
     """
     <div style='text-align: center; font-size: small; color: gray;'>
     © 2023 Jonathan CAMBON, Didier FLAMM, Mamadou HASSIMIOU BAH, Ilyass SAIDI, supervised by Khalil OUERTANI<br>
-    © 2025 Didier FLAMM, Pavithra BHASKAR
+    © 2025 Didier FLAMM feat. Pavithra BHASKAR
     </div>
     """,
     unsafe_allow_html=True,
