3fdcead8c162919e155692617e98966564ad382b
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-07-23 00:39:45 +0200

    big update

--- pages/4_ml.py
+++ pages/4_ml.py
@@ -1,18 +1,35 @@
 import streamlit as st
-
+from utils import trans_write
+import pandas as pd
 
 st.markdown(
     "<h2 style='text-align: center; color: #DC143C;'>🤖 Machine Learning</h2>",
     unsafe_allow_html=True,
 )
 
-st.write("3 models: XGBoost, Random Forest and SVM")
 
-st.subheader("📈 :red[Results]", divider=True)
+text_0 = "😬 To our great regret, we will not be able to give you a live demonstration of the training and fine-tuning of the models on the training set, for two reasons:"
 
-st.write(
-    "La binarization n'a pas améliorer les performances pcq les cellules étaient déjà colorisées donc facilement identifiables par les modèles..."
+text_1 = "1. Even resized to 100x100 pixels, RGB image has 30,000 features. With 17,000 images, the model must process over 500 million data points. Traditional Machine Learning trains in a reasonable time but Deep Learning, excelling in feature extraction, requires much longer training times (tens of minutes to hours)."
+text_2 = "2. Streamlit Cloud cannot host the full 300MB dataset, nor download it fast enough from an external host to provide a smooth user experience."
+text_3 = "As a result, we will be showcasing only the results from the training and fine-tuning of the 3 models that was completed in advance."
+
+for text in [text_0, text_1, text_2, text_3]:
+    trans_write(text, st.session_state.lang.split("-")[0])
+
+results = pd.DataFrame(
+    {
+        "Model": ["Random Forest", "SVM", "XGBoost"],
+        "Accuracy without Binarization": ["85%", "83%", "91%"],
+        "Accuracy after Binarization": ["73%", "57%", "74%"],
+    }
 )
+results.set_index("Model", inplace=True)
+
+st.dataframe(results)
+
+text = "🧐 We find that binarization did not deliver the expected results: while training was much faster, the performance loss does not justify its use. The best-performing Machine Learning model is XGBoost after training on the raw images."
+trans_write(text, st.session_state.lang.split("-")[0])
 
 _, _, col = st.columns(3)
 with col:
